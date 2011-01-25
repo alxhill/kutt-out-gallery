1b2396698419394e14006af95e4b5fc2e7f47ca6
Alexander Hill <me@alxhill.com>
2011-01-25 12:46:36 +0000

    Added firefox fixes - login page is broken.

--- application/views/gallery/common/header.php
+++ application/views/gallery/common/header.php
@@ -54,7 +54,8 @@ $(document).ready(function(){
 <?php } ?>
 <div class="container">
 <?=isset($message) ? show_messages($class,$message) : null ?>
-<div class='span-8' id='nav'>
+<div id='nav'>
+<div id='mainbg'>
 	<ul>
 		<li><a href='<?=site_url('/home')?>'><div id='kutt'>Kutt</div><div id='out'>Out</div><div id='studios'>Studios</div></a></li>
 		<li><a href='/gallery/portraits'>Portraits</a></li>
@@ -64,4 +65,5 @@ $(document).ready(function(){
 		<li><a href='#'>Contact</a></li>
 		<li><a href='#'><div id='billy'>Billy</div><div id='boyd'>Boyd</div><div id='cape'>Cape</div></a></li>
 	</ul>
+</div>
 </div>
